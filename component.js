645f29c68e74da984bba1264e184c11cf5283f82
AaronRodrigues <aaronrod@gmail.com>
2017-09-06 19:12:12 +0100

    Adds component.js

---
+++ component.js
@@ -0,0 +1,25 @@
+mport React from 'react';
+
+import ReactDOM from 'react-dom';
+
+
+
+class MyComponentClass extends React.Component {
+
+  render(){
+
+    return <h1>Hello world</h1>;  
+
+  }
+
+};
+
+
+
+ReactDOM.render(
+
+<MyComponentClass />,
+
+document.getElementById('app')
+
+);
