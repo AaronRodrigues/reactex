8aa6557fdd7cd066506feaccf15961db5c50d13d
AaronRodrigues <aaronrod@gmail.com>
2017-09-06 18:52:25 +0100

    Adds keys

---
+++ keys.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+
+
+const peopleLis = people.map((person, i) =>
+
+  // expression goes here:
+
+<li key={"person_" + i}>{person}</li>
+
+);
+
+
+
+  <ul>{peopleLis}</ul>
+
+// ReactDOM.render goes here:
+
+ReactDOM.render(<ul>{peopleLis}</ul>, document.getElementById('app'));
