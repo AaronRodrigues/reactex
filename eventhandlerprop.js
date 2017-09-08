cd5953b15ea4507e5b1e13cc9c5fee01a3824fe1
AaronRodrigues <aaronrod@gmail.com>
2017-09-08 12:58:28 +0100

    event handlers and props

---
+++ eventhandlerprop.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import { Button } from './Button';
+
+
+
+class Talker extends React.Component {
+
+  handleClick() {
+
+    let speech = '';
+
+    for (let i = 0; i < 10000; i++) {
+
+      speech += 'blah ';
+
+    }
+
+    alert(speech);
+
+  }
+
+  
+
+  render() {
+
+    return <Button talk={this.handleClick} />;
+
+  }
+
+}
+
+
+
+ReactDOM.render(
+
+  <Talker />,
+
+  document.getElementById('app')
+
+);
